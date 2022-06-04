e0ac7883ee2cb65c054353b8f3f9539e13514ca1
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2022-06-03 20:26:41 -0500

    added itachi's image, deleted scss navbar, variables were added to home and burgermenu

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from 'styled-components'
+import {colorPurple, colorWhite, colorSilver, colorBone, colorBlack} from '../assets/styles/abstracts/variables'
+
 
 function BurgerMenu(props) {
     return (
@@ -29,7 +31,7 @@ const Burger = styled.div`
     display: inline-block;
     }
     .nav-icon-5 span{
-    background-color:#fff;
+    background-color: ${colorWhite};
     position: absolute;
     border-radius: 2px;
     transition: .3s cubic-bezier(.8, .5, .2, 1.4);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from 'styled-components'
+import myPicture from '../assets/img/itachi.jpg'
+import {colorPurple, colorWhite, colorSilver, colorBone, colorBlack} from '../assets/styles/abstracts/variables'
+
 
 function Home() {
     return (
         <Me>
             <h2>Erick Vergara</h2>
+            <img src={myPicture} alt="itachi" />
         </Me>
     )
 }
@@ -14,8 +18,20 @@ export default Home
 
 const Me = styled.section `
     background-color: #7300ff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img{
+        width: 4rem;
+    }
+
     h2{
         color: white;
         padding: .4rem;
+    }
+
+    @media(min-width: 768px){
+        display: flex ;
     }
 `
