f2c17d43a68182127dba867cc6510d1199b86b9a
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2022-06-04 20:29:02 -0500

    profile design, skills design

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,9 +7,18 @@ function Profile() {
     return (
         <AboutMe>
             <div className="aboutContainer">
-                <h3>My name is Erick Vergara</h3>
+                <h2>My name is Erick Vergara</h2>
                 <p>I'm a Frontend developer with knowledge in many languages such as React or JavaScript.
                     I have many soft skills such as ........
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a efficitur augue. Nam dictum turpis ut venenatis pulvinar. 
+                    Phasellus viverra eros fringilla orci feugiat tristique. Donec eu sem orci. Donec eget tristique purus, vel mollis lorem. 
+                    Praesent non urna sit amet augue gravida bibendum sed consequat lectus. Integer nisl lacus, 
+                    sodales in quam eget, maximus elementum dui. Donec condimentum malesuada sapien, 
+                    in volutpat nunc suscipit nec. Proin sed iaculis orci,
+                    vel sollicitudin neque. Aliquam ultricies accumsan orci non pretium.
+                    Integer mi enim, malesuada hendrerit lacus ut, tempus auctor ligula.
                 </p>
                 <button> Download CV</button>
             </div>
@@ -22,7 +31,7 @@ export default Profile
 
 const AboutMe = styled.section `
     background-color: ${colorPurple};
-    color: ${colorBone};
+    color: ${colorWhite};
     padding-top: 2rem;
     padding-bottom: 14rem;
 
@@ -31,23 +40,39 @@ const AboutMe = styled.section `
         align-items: center;
 
     .aboutContainer{
+        line-height: 1.5;
+        margin: 2rem;
         display: flex;
         flex-direction: column;
         align-items: center;
         width: 60%;
+        h2{
+            @media(min-width: 768px){
+                font-size: 32px;
+            }
+
+        }
 
         p{
             text-align: center;
+
+            @media(min-width: 768px){
+                font-size: 20px;
+            }
         }
     }
 
 
     button{
-        background-color: ${colorSilver};
+        background-color: ${colorPurple};
         color: ${colorBone};
         width: 8rem;
         align-self: center;
         margin: 1rem;
+
+        border: 3px solid;
+        border-radius: 5px;
+        padding: 5px;
     }
 
     @media(min-width: 768px){
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import styled from 'styled-components'
+import {colorPurple, colorWhite, colorSilver, colorBone, colorBlack} from '../assets/styles/abstracts/variables'
+
 
 function Skills() {
     return (
-        <div> Este es mi Skills </div>
+        <SkillsContainer>
+            <div className="box">
+                <h2>Skills</h2>
+                <div className="carrousel">
+                    <p>React</p>
+                    <p>JavasCript</p>
+                </div>
+
+            </div>
+        </SkillsContainer>
     )
 }
 
 
 export default Skills
+
+
+const SkillsContainer = styled.section`
+    background-color: ${colorWhite};
+    display: flex;
+    justify-content: center;
+    .box{
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: absolute;
+        background-color: ${colorBone};
+        top: 1300px;
+        width: 90%;
+        height: 50%;
+
+        box-shadow: 0 5px 5px 0 rgb(233 240 243 / 50%), 0 0 0 1px #e6ecf8;
+    }
+`
