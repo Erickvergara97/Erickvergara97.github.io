6decddb89482ff87a29d749d7dd6754fff723dfc
Erick Vergara <erick.vergara.ruiz@gmail.com>
2024-05-26 23:17:58 -0500

    contact icons style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,28 +3,24 @@ import styled from 'styled-components'
 import {SiLinkedin, SiGmail, SiGithub } from "react-icons/si"
 
 
-function Contact() {
+function Contact({isDarkMode}) {
     return (
-        <ContactContainer id="contact">
-            <h2>Contact </h2>
+        <ContactContainer id="contact" isDarkMode={isDarkMode}>
             <div className="box">
                 <div className="miniBox">
                     <a href="mailto:erick.vergara.ruiz@gmail.com" target="_blank" rel="noreferrer">
                         <SiGmail className="icon gmail"/>
                     </a>
-                    <p>Gmail</p>
                 </div>
                 <div className="miniBox">
                     <a href="https://www.linkedin.com/in/erick-vergara-a782691a5/" target="_blank" rel="noreferrer">
                         <SiLinkedin className="icon linkedin"/>
                     </a>
-                    <p>Linkedn</p>
                 </div>
                 <div className="miniBox">
                 <a href="https://github.com/Erickvergara97" target="_blank" rel="noreferrer">
                     <SiGithub className="icon github"/>
                 </a>
-                    <p>Github</p>
                 </div>
             </div>
 
@@ -47,7 +43,7 @@ const ContactContainer = styled.section`
     .box{
         display: flex;
         flex-flow: row wrap;
-        justify-content: space-between;
+        justify-content: center;
 
         @media(min-width: 768px){
             width: 60%;
@@ -63,16 +59,23 @@ const ContactContainer = styled.section`
             }
 
             .icon{
-                width: 4rem;
+                width: 3rem;
                 height: auto;
             }
 
-            .gmail{ color: red}
-            .linkedin{ color: #006192}
-            .github{ color: #211F1F}
+            .gmail{
+                ${({ isDarkMode, theme }) => (`color: ${isDarkMode ? theme.icons : 'red'}`)}
+            }
+            .linkedin{ 
+                ${({ isDarkMode, theme }) => (`color: ${isDarkMode ? theme.icons : '#006192'}`)}
+            }
+            .github{ 
+                ${({ isDarkMode, theme }) => (`color: ${isDarkMode ? theme.icons : '#211F1F'}`)}
+            }
 
             .gmail, .linkedin, .github {
-                transition: color 2s, background-color 2s;
+                transition: color 1s, background-color 1s;
+
                 &:hover{
                     color: ${({ theme }) => theme.accent };
                     background-color: transparent;
